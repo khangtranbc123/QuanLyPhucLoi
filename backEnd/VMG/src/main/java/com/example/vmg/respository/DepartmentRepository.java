87b81b34afe9093212ca34fefa0f9120c8045f6b
phongdhhe151364 <phongdhhe151364@fpt.edu.vn>
2022-11-01 22:15:48 +0700

    fix

--- backEnd/VMG/src/main/java/com/example/vmg/respository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.vmg.respository;
-
-import com.example.vmg.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    public Department getDepartmentByName(String name);
-}
--- backEnd/VMG/src/main/java/com/example/vmg/respository/GeneralWelfareRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.vmg.respository;
-
-import com.example.vmg.model.GeneralWelfare;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeneralWelfareRepository extends JpaRepository<GeneralWelfare, Long> {
-}
